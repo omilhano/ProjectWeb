ab4ec0153978d076030b5a8235ea0e963cb52d49
GuiAlvesMarques <36242349+GuiAlvesMarques@users.noreply.github.com>
2023-05-10 11:43:19 +0100

    Update HomePageJS.js

--- javascript/HomePageJS.js
+++ javascript/HomePageJS.js
@@ -1,4 +1,4 @@
- $(".slideshow").cycle({fx:"scrollHorz"}); //adiciona efeito de scroll horizontal para a esquerda
+  $(".slideshow").cycle({fx:"scrollHorz"}); //adiciona efeito de scroll horizontal para a esquerda
 
      $(document).ready(function() {
           $(".slideshow").cycle();
@@ -62,3 +62,42 @@
      });
      }
      google.maps.event.addDomListener(window, "load", initMap);
+
+
+// Get the modal
+var modal = document.getElementById("file_modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("login-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        }
+// Checks if password corresponds to the user
+function checkPassword() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (password === "mypassword") {
+        // correct password, do something
+    } else {
+        // incorrect password, update error message
+        var errorElement = document.getElementById("password-error");
+        errorElement.textContent = "Incorrect password. Please try again.";
+    }
+}
