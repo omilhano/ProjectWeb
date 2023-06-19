0dfb2d7f5569f689b237ed106215c3169e913c8a
Rafael do Espírito Santo Ribeiro <126074740+Sachris04@users.noreply.github.com>
2023-06-19 15:41:50 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -11,30 +11,41 @@ if (@$_REQUEST['logout'] == 1) {
 if (@$_POST['username'] && @$_POST['password']) {
     $uuser = mysqli_real_escape_string($link, $_POST['username']);
     $inputPassword = ($_POST['password']);
-
-    $sql = "SELECT password, admin FROM user WHERE email='$uuser'";
+    
+    $sql = "SELECT Password, Admin, Email, Username FROM user WHERE email='$uuser'";
     $result = mysqli_query($link, $sql);
+    
 
     if (!$result || mysqli_num_rows($result) < 1) {
         exit;
     } else {
         $row = mysqli_fetch_assoc($result);
-        $storedPassword = $row['password'];
-        $isAdmin = $row['admin'];
-
+        $storedPassword = $row['Password'];
+        $isAdmin = $row['Admin'];
+        
         if ($inputPassword != $storedPassword) {
             print('hello');
             exit;
         }
 
         $_SESSION['username'] = $uuser;
-
+        //After validate the password, get the user_id
+        $sql = "SELECT id FROM user WHERE email='$uuser'";
+        $result = mysqli_query($link, $sql);
+        
+        if (!$result || mysqli_num_rows($result) < 1) {
+            exit;
+        } else {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['user_id'] = $row['id']; 
+            } 
+        
         if ($isAdmin == 1) {
             // Redirect to the admin page
-            header("Location: HomePageAdmin.html");
+            header("Location: ../html/HomePageAdmin.html");
         } else {
             // Redirect to the regular user page
-            header("Location: HomePageLogIN.html");
+            header("Location: ../html/HomePageLogIN.html");
         }
 
         exit;
