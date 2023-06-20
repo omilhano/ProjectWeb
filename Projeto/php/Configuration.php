857922cce2e6e7764b4a2bb294066d57a42c7995
GuiAlvesMarques <36242349+GuiAlvesMarques@users.noreply.github.com>
2023-06-20 19:08:05 +0100

    Add files via upload

---
+++ Projeto/php/Configuration.php
@@ -0,0 +1,7 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "travelwebsite2");
+
+//chech connection
+if($link == false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
