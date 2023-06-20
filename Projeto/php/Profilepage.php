12bba5494c25c582d0598dd7b2afa1c786b12a6f
Rafael do Espírito Santo Ribeiro <126074740+Sachris04@users.noreply.github.com>
2023-06-20 22:23:35 +0100

    Update Profilepage.php

--- Projeto/php/Profilepage.php
+++ Projeto/php/Profilepage.php
@@ -99,7 +99,7 @@ if (isset($_SESSION['username'])) {
   <title>Turistic</title>
   <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
   <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap' rel='stylesheet'>
-  <link rel="stylesheet" href="../css/profilepage.css">
+  <link rel="stylesheet" href="../css/Profilepage.css">
   <link rel="stylesheet" href="../css/footer.css">
   <link rel="stylesheet" href="../css/navbar.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
